60b44aecc1c3cec651a6746340534a314344b4d2
hina1gt <mahinatimurovna@gmail.com>
2023-09-21 21:37:04 +0500

    new

--- src/settings.py
+++ src/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-!(xbc4fm6n$ufov2kuu&ze!90mv875h88vm1y4+-kj*5o+i4a_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
